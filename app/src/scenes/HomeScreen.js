1ae82d9a459b78852e60d60c94b12a3ef60d2d6b
Rishikesh Gaikar <rishikeshgaikar07@gmail.com>
2019-10-25 18:00:29 +0530

    updated homescreen

--- app/src/scenes/HomeScreen.js
+++ app/src/scenes/HomeScreen.js
@@ -21,139 +21,134 @@ import { connect } from 'react-redux';
 import { apiWeather } from '../redux/Actions';
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 Icon.loadFont();
-const HomeScreen = ({
-  loc,
-  lat,
-  lon,
-  wetData,
-  loading,
-  unit,
-  lang,
-  iconColor,
-  navigation,
-  apiWeather
-}) => {
-  useEffect(() => {
-    const isFocused = navigation.isFocused();
-    if (isFocused) {
-      apiCall();
-    }
-    const navFocusListener = navigation.addListener('didFocus', () => {
-      apiCall();
+
+class HomeScreen extends Component {
+  componentDidMount() {
+    this.onFocusCall = this.props.navigation.addListener('willFocus', () => {
+      this.apiCall();
     });
-
-    return () => {
-      navFocusListener.remove();
-    };
-  }, []);
-
-  const apiCall = () => {
-    apiWeather(lat, lon, unit, lang);
-  };
-
-  if (!loading) {
-    return (
-      <RootView>
-        <View
-          style={{
-            flexDirection: 'row'
-          }}
-        >
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center'
-            }}
-          >
-            <TouchableOpacity
-              style={{ padding: 15, alignSelf: 'flex-start' }}
-              onPress={() => apiCall()}
-            >
-              <Icon name={'refresh'} size={30} color={iconColor} />
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              paddingTop: 17.5,
-              flex: 4,
-              alignItems: 'center'
-            }}
-          >
-            <ScrollView
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-            >
-              <CText title={loc} fontsize={20} />
-            </ScrollView>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center'
-            }}
-          >
-            <TouchableOpacity
-              style={{ padding: 15, alignSelf: 'flex-end' }}
-              onPress={() => navigation.navigate('SettingScreen')}
-            >
-              <Icon name={'settings'} size={30} color={iconColor} />
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.cc1}>
-          <View style={styles.c1}>
-            <Card>
-              <View
-                style={{
-                  flex: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}
-              >
-                <Img d2={wetData.currently.icon} h={125} w={125} />
-
-                <Text style={{ fontSize: 60 }}>
-                  {Math.round(wetData.currently.temperature) +
-                    (unit == 'si' ? ' \u2103' : ' \u2109')}
-                </Text>
-                <Text style={{ fontSize: 20 }}>
-                  {wetData.currently.summary}
-                </Text>
-              </View>
-            </Card>
-          </View>
-          <View style={styles.c2}>
-            <CurrentlyCardRow
-              pressure={wetData.currently.pressure}
-              windSpeed={wetData.currently.windSpeed}
-              humidity={wetData.currently.humidity}
-              dewPoint={wetData.currently.dewPoint}
-              uvIndex={wetData.currently.uvIndex}
-              visibility={wetData.currently.visibility}
-              ozone={wetData.currently.ozone}
-            />
-          </View>
-        </View>
-        <View style={styles.cc2}>
-          <FlatList
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            data={wetData.daily.data}
-            renderItem={({ item }) => <WeeklyCard data={item} />}
-            keyExtractor={item => item.id}
-          />
-        </View>
-      </RootView>
-    );
-  } else {
-    return (
-      <RootView>
-        <Loading />
-      </RootView>
+  }
+  componentWillUnmount() {
+    onFocusCall.remove();
+  }
+  apiCall() {
+    this.props.apiWeather(
+      this.props.lat,
+      this.props.lon,
+      this.props.unit,
+      this.props.lang
     );
   }
-};
+  render() {
+    if (!this.props.loading) {
+      const { wetData } = this.props;
+      return (
+        <RootView>
+          <View
+            style={{
+              flexDirection: 'row'
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <TouchableOpacity
+                style={{ padding: 15, alignSelf: 'flex-start' }}
+                onPress={() => this.apiCall()}
+              >
+                <Icon name={'refresh'} size={30} color={this.props.iconColor} />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                paddingTop: 17.5,
+                flex: 4,
+                alignItems: 'center'
+              }}
+            >
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+              >
+                <CText title={this.props.loc} fontsize={20} />
+              </ScrollView>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center'
+              }}
+            >
+              <TouchableOpacity
+                style={{ padding: 15, alignSelf: 'flex-end' }}
+                onPress={() => this.props.navigation.navigate('SettingScreen')}
+              >
+                <Icon
+                  name={'settings'}
+                  size={30}
+                  color={this.props.iconColor}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.cc1}>
+            <View style={styles.c1}>
+              <Card>
+                <View
+                  style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }}
+                >
+                  <Img d2={wetData.currently.icon} h={125} w={125} />
+
+                  <Text style={{ fontSize: 60 }}>
+                    {Math.round(wetData.currently.temperature) +
+                      (this.props.unit == 'si' ? ' \u2103' : ' \u2109')}
+                  </Text>
+                  <Text style={{ fontSize: 20 }}>
+                    {wetData.currently.summary}
+                  </Text>
+                </View>
+              </Card>
+            </View>
+            <View style={styles.c2}>
+              <CurrentlyCardRow
+                pressure={wetData.currently.pressure}
+                windSpeed={wetData.currently.windSpeed}
+                humidity={wetData.currently.humidity}
+                dewPoint={wetData.currently.dewPoint}
+                uvIndex={wetData.currently.uvIndex}
+                visibility={wetData.currently.visibility}
+                ozone={wetData.currently.ozone}
+              />
+            </View>
+          </View>
+          <View style={styles.cc2}>
+            <FlatList
+              showsHorizontalScrollIndicator={false}
+              horizontal={true}
+              data={wetData.daily.data}
+              renderItem={({ item }) => <WeeklyCard data={item} />}
+              keyExtractor={item => item.id}
+            />
+          </View>
+        </RootView>
+      );
+    } else {
+      return (
+        <RootView>
+          <Loading />
+        </RootView>
+      );
+    }
+  }
+}
 
 const mapStateToProps = ({ mainR, apiR, langR, unitR, themeR }) => {
   const { loc, lat, lon } = mainR;
